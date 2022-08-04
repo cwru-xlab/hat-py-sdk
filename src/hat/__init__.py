22f32413b72671a1d49a335a594b1a5542ff24b3
rtatton <rdt17@protonmail.com>
2022-08-04 00:37:29 -0400

    Update __init__ imports
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/hat/__init__.py
+++ src/hat/__init__.py
@@ -1,8 +1,8 @@
-from .client import HatClient, HatRecord
+from .client import HatClient
 from .errors import (AuthError, DeleteError, DuplicateDataError, GetError,
                      HatError, HatNotFoundError, LimitedTokenScopeError,
                      MalformedBodyError, MissingPathError, PostError, PutError,
                      RecordNotFoundError, WrongCredentialsError,
                      WrongTokenError)
-from .models import GetOpts, HatModel, HatRecord, Ordering
+from .models import GetOpts, HatModel, M, Ordering
 from .tokens import ApiOwnerToken, AppToken, OwnerToken, Token, WebOwnerToken
