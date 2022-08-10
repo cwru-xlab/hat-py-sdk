42f9af81152e1274ca9f445ebbac2b503005f620
rtatton <rdt17@protonmail.com>
2022-08-10 14:24:56 -0400

    Fix imports
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/hat/active.py
+++ src/hat/active.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import ClassVar, Iterable, Optional, TypeVar
 
-import errors
+from . import errors
 from .client import HatClient, M, StringLike
 from .model import GetOpts, HatModel
 
