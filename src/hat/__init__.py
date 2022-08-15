e9b82a3919a916e18c2175d597f5565a7215a97a
rtatton <rdt17@protonmail.com>
2022-08-15 12:25:32 -0400

    Update __init__.py
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/hat/__init__.py
+++ src/hat/__init__.py
@@ -2,8 +2,8 @@ from .active import A, ActiveHatModel
 from .auth import (ApiToken, AppToken, AsyncApiToken, AsyncAppToken,
                    AsyncCredentialOwnerToken, AsyncTokenAuth,
                    AsyncWebOwnerToken, CredentialAuth, CredentialOwnerToken,
-                   WebOwnerToken)
-from .client import HatClient
+                   JwtAppToken, JwtOwnerToken, JwtToken, WebOwnerToken)
+from .client import AsyncHatClient, HatClient
 from .errors import (AuthError, DeleteError, DuplicateDataError, GetError,
                      HatError, HatNotFoundError, LimitedTokenScopeError,
                      MalformedBodyError, MissingPathError, PostError, PutError,
