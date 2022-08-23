ca6429772831ed8110c57226c959116b2d00ecdb
rtatton <rdt17@protonmail.com>
2022-08-23 14:37:41 -0400

    Add ErrorMapping builder; fix UnsupportedMediaTypeError
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/hat/errors.py
+++ src/hat/errors.py
@@ -5,11 +5,14 @@ from typing import Any
 from typing import Callable
 from typing import Generic
 from typing import Optional
-from typing import Type
 from typing import TypeVar
 
 
 class HatError(Exception):
+    pass
+
+
+class UnsupportedMediaTypeError(HatError):
     pass
 
 
@@ -67,45 +70,57 @@ class LimitedTokenScopeError(GetError, PostError, PutError, DeleteError):
     pass
 
 
-class UnsupportedMediaTypeError(PostError, PutError):
-    pass
-
-
 E = TypeVar("E", bound=Exception)
-Resolver = Callable[[Any], Type[E]]
-V = tuple[Optional[Type[E]], Optional[Resolver]]
+Resolver = Callable[[Any], type[E]]
+V = tuple[Optional[type[E]], Optional[Resolver]]
 
 
 class ErrorMapping(Generic[E]):
     __slots__ = "_default", "_errors"
 
-    def __init__(self, default: type[E]):
-        self._default = default
-        self._errors = self._new_map(default)
+    def __init__(self, builder: Builder[E]):
+        self._default = builder.default
+        self._errors = builder.errors
 
-    @staticmethod
-    def _new_map(default: type[E]) -> dict[int, V]:
-        return collections.defaultdict(lambda: (default, None))
+    @classmethod
+    def builder(cls, default: type[E]) -> Builder[E]:
+        return ErrorMapping.Builder(default)
 
     def get(self, status: int, content: Any) -> type[E]:
         error, resolver = self._errors[status]
         return error if resolver is None else resolver(content)
 
-    def put(
-        self,
-        status: int,
-        error: type[E] | None = None,
-        resolver: Resolver | None = None,
-    ) -> None:
-        if not (error is None) ^ (resolver is None):
-            raise ValueError("Either 'error' or 'resolver' must be specified")
-        self._errors[status] = (error, resolver)
-
-    def update(self, mapping: ErrorMapping) -> None:
-        self._errors.update(mapping._errors)
-
     def default(self) -> type[E]:
         return self._default
+
+    class Builder(Generic[E]):
+        __slots__ = "default", "errors"
+
+        def __init__(self, default: type[E]):
+            self.default = default
+            self.errors = self._new_map(default)
+
+        @staticmethod
+        def _new_map(default: type[E]) -> dict[int, V]:
+            return collections.defaultdict(lambda: (default, None))
+
+        def put(
+            self,
+            status: int,
+            error: type[E] | None = None,
+            resolver: Resolver | None = None,
+        ) -> ErrorMapping.Builder[E]:
+            if not (error is None) ^ (resolver is None):
+                raise ValueError("Either 'error' or 'resolver' must be specified")
+            self.errors[status] = (error, resolver)
+            return self
+
+        def put_all(self, mapping: ErrorMapping[E]) -> ErrorMapping.Builder[E]:
+            self.errors.update(mapping._errors)
+            return self
+
+        def build(self) -> ErrorMapping[E]:
+            return ErrorMapping(self)
 
 
 def _resolve_put_400(content: dict[str, str] | str) -> type[PutError]:
@@ -121,33 +136,46 @@ def _resolve_put_400(content: dict[str, str] | str) -> type[PutError]:
 
 POSSIBLE_CODES = (400, 401, 403, 404, 415, 500)
 
-auth_errors = ErrorMapping(AuthError)
-auth_errors.put(401, WrongCredentialsError)
-auth_errors.put(404, HatNotFoundError)
+auth_errors = (
+    ErrorMapping.builder(AuthError)
+    .put(401, WrongCredentialsError)
+    .put(404, HatNotFoundError)
+    .build()
+)
 
-crud_errors = ErrorMapping(HatError)
-crud_errors.put(401, WrongTokenError)
-crud_errors.put(403, LimitedTokenScopeError)
+crud_errors = (
+    ErrorMapping.builder(HatError)
+    .put(401, WrongTokenError)
+    .put(403, LimitedTokenScopeError)
+    .put(415, UnsupportedMediaTypeError)
+    .build()
+)
 
-get_errors = ErrorMapping(GetError)
-get_errors.update(crud_errors)
+get_errors = ErrorMapping.builder(GetError).put_all(crud_errors).build()
 
-post_errors = ErrorMapping(PostError)
-post_errors.update(crud_errors)
-post_errors.put(400, DuplicateDataError)
-post_errors.put(415, UnsupportedMediaTypeError)
+post_errors = (
+    ErrorMapping.builder(PostError)
+    .put_all(crud_errors)
+    .put(400, DuplicateDataError)
+    .build()
+)
 
-put_errors = ErrorMapping(PutError)
-put_errors.update(crud_errors)
-put_errors.put(400, resolver=_resolve_put_400)
-put_errors.put(415, UnsupportedMediaTypeError)
-put_errors.put(500, DuplicateDataError)
+put_errors = (
+    ErrorMapping.builder(PutError)
+    .put_all(crud_errors)
+    .put(400, resolver=_resolve_put_400)
+    .put(500, DuplicateDataError)
+    .build()
+)
 
-delete_errors = ErrorMapping(DeleteError)
-delete_errors.update(crud_errors)
-delete_errors.put(400, RecordNotFoundError)
+delete_errors = (
+    ErrorMapping.builder(DeleteError)
+    .put_all(crud_errors)
+    .put(400, RecordNotFoundError)
+    .build()
+)
 
-all_errors: dict[str, ErrorMapping] = {
+all_errors: dict[str, ErrorMapping[HatError]] = {
     "auth": auth_errors,
     "get": get_errors,
     "post": post_errors,
